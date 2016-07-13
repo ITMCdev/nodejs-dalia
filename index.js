59c24312edbbaaf0410615f7c99878f62db567a0
Dragos Cirjan <Dragos.Cirjan@pj-interactive.ro>
2016-07-13 18:13:48 +0300

    +

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+/**
+ * https://stackoverflow.com/questions/32429488/how-to-redirect-crawlers-requests-to-pre-rendered-pages-when-using-amazon-s3
+ * http://phantomjs.org/
+ * https://github.com/localnerve/html-snapshots
+ * https://github.com/lukekarrys/spa-crawler
+ * http://stackoverflow.com/questions/28916699/is-there-a-way-to-listen-to-an-event-in-the-phantom-context-from-page-context
+ * https://snippets.aktagon.com/snippets/534-how-to-scrape-web-pages-with-phantomjs-and-jquery
+ */
+
+/**
+ * for test: 
+ * http://aurelia.io/hub.html#/doc/api
+ * https://docs.angularjs.org/api
+ */
