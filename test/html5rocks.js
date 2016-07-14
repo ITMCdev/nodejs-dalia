aa416f24496b84ca06012f37328815334ed4af20
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2016-07-14 17:47:32 +0300

    +

--- test/html5rocks.js
+++ test/html5rocks.js
@@ -3,6 +3,6 @@ var sophia = require('../index');
 var assert = require('assert');
 
 sophia.parseUrls('http://www.html5rocks.com').then(
-    function(data) { console.log(sophia.flushUrls()); },
+    function(data) { console.log(data); },
     function(err) { assert.fail(err); }
 );
