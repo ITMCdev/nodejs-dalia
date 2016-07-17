4c1abc1a87e6b698c07680f1e4b51000322c67ef
Dragos Cirjan <dragos.cirja@itmediaconnect.ro>
2016-07-18 01:17:16 +0300

    +

--- test/getbootstrap.js
+++ test/getbootstrap.js
@@ -2,7 +2,7 @@
 var sophia = require('../index');
 var assert = require('assert');
 
-sophia.parseUrls('http://getbootstrap.com', {}, { match: /getbootstrap.com/i }).then(
+sophia.parseUrls('http://getbootstrap.com', 10, { match: /getbootstrap.com/i }).then(
     function(data) { console.log(data); },
     function(err) { assert.fail(err); }
 );
