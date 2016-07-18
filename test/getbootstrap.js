31f9f5619f4741c5101b5ca65fea6c47b5846b50
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2016-07-18 17:38:17 +0300

    fixed url indexing, moved to sitemap

--- test/getbootstrap.js
+++ test/getbootstrap.js
@@ -3,7 +3,7 @@ var sophia = require('../index');
 var assert = require('assert');
 
 var options = { match: /\/getbootstrap.com/i };
-sophia.parseUrls('http://getbootstrap.com', 10, options).then(
+sophia.parseUrls('http://getbootstrap.com', options).then(
     function(data) { console.log(data); console.log(options.found); },
     function(err) { assert.fail(err); }
 );
--- test/html5rocks.js
+++ test/html5rocks.js
@@ -2,22 +2,18 @@
 var sophia = require('../index');
 var assert = require('assert');
 
-// sophia.parseUrls('http://www.html5rocks.com').then(
+var options = { match: /^http(s?):\/\/(www|updates).html5rocks.com/i, maxDepth: 2 };
+// var options = { match: /www.html5rocks.com/i, maxDepth: 2 };
+
+// sophia.parseUrls('http://www.html5rocks.com', options).then(
 //     function(data) { console.log(data); },
 //     function(err) { assert.fail(err); }
 // );
 
-// sophia.parseUrls('http://www.html5rocks.com', {}, { match: /www.html5rocks.com/i }).then(
-//     function(data) { console.log(data); },
-//     function(err) { assert.fail(err); }
-// );
+options.maxDepth = 1;
+sophia.siteMap('http://www.html5rocks.com', options);
 
-sophia.parseUrls('http://www.html5rocks.com', 2, { match: /html5rocks.com/i }).then(
-    function(data) { console.log(data); },
-    function(err) { assert.fail(err); }
-);
-
-// sophia.parseUrls('http://casacontelui.ro', 100, { match: /casacontelui.ro/i }).then(
+// sophia.parseUrls('http://casacontelui.ro', { match: /casacontelui.ro/i, maxDepth: 2 }).then(
 //     function(data) { console.log(data); },
 //     function(err) { assert.fail(err); }
 // );
